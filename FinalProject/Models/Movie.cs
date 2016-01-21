84995d4e55d8b0a812aaba1bf49dd4fdca77ba60
suiname <jtham@sbcglobal.net>
2016-01-20 23:12:29 -0600

    added globalize jquery validator, added to edit form

--- FinalProject/Models/Movie.cs
+++ FinalProject/Models/Movie.cs
@@ -8,6 +8,10 @@ namespace FinalProject.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
